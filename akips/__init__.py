45aa5d8f8c484f473c01615a62a50213d1f09442
William E Whitaker <will.whitaker@unc.edu>
2025-04-01 13:33:57 -0400

    version bump to 0.4.2

--- akips/__init__.py
+++ akips/__init__.py
@@ -1,7 +1,7 @@
 """This akips python module provides a simple way for python scripts to interact with
 the AKiPS Network Monitoring Software Web API interface."""
 
-__version__ = "0.4.1"
+__version__ = "0.4.2"
 
 import io
 import re
